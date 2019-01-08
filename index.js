5245034d7b092fdfdce1484f013430b42fb34ec8
Elizabeth Chan <lchan217@gmail.com>
2019-01-08 21:43:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
-function shout(string){
-  return (string.toUpperCase())
+function shout (string){return string.toUpperCase()}
+function whisper (string){return string.toLowerCase()}
+function logShout(string){console.log(string.toUpperCase())}
+function logWhisper(string){console.log(string.toLowerCase())}
+function sayHiToGrandma (string) {
+<<<<<<< HEAD
+  if(string===string.toLowerCase()){return "I can't hear you!"}
+  else if(string===string.toUpperCase()){return "YES INDEED!"}
+  else {return "I love you, too."}
 }
+=======
+  if(string===string.toLowerCase){return "I can't hear you!"}
+  else if(string===string.toUpperCase){return "YES INDEED!"}
+  else {return "I love you, too"}
+>>>>>>> 787a464b6d1536ce32c1730ca31a88fb90765d0d
 
-function whisper(string){
-  return (string.toLowerCase())
-}
-
-function logShout(string){
-  console.log(string.toUpperCase())
-}
-
-function logWhisper(string){
-  console.log(string.toLowerCase())
-}
-
-function sayHiToGrandma(string){
-  if(string===string.toLowerCase()){
-    return ("I can't hear you!")
-  }else if(string===string.toUpperCase()){
-    return ("YES INDEED!")
-  } else {
-  return   "I love you, too."
-  }
-}
